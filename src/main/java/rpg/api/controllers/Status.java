c82a2f363aa0ba64f965a33e47bbc7b665fa7dc6
oponei <56514281+OJoaoSP@users.noreply.github.com>
2023-08-20 19:53:58 -0300

    add Status.java

---
+++ src/main/java/rpg/api/controllers/Status.java
@@ -0,0 +1,12 @@
+package rpg.api.controllers;
+
+public class Status {
+    private int Forca;
+    private int Destreza;
+    private int Constituicao;
+    private int Inteligencia;
+    private int Sabedoria;
+    private int Carisma;
+
+
+}
